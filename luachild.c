154d2349b1d68dcbd1620b72f18af07983c69b41
fsfod <fsfod11@gmail.com>
2018-02-12 14:01:42 +0000

    Just always build for Lua 5.1 to stay compatible mostly with all Lua versions

--- luachild.c
+++ luachild.c
@@ -9,11 +9,7 @@
 
 #include "lualib.h"
 
-#ifdef LUA_JITLIBNAME // Match luajit
 #include "luachild_luajit_2_1.c"
-#else // fallback to PUC-Rio lua
-#include "luachild_lua_5_3.c"
-#endif
 
 #include "luachild_common.c"
 
